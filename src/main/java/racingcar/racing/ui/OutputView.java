23bc9d34e6d08cf07159f6178ef37aefc19a3333
junwoo <mkshie@naver.com>
2025-10-27 19:37:10 +0900

    feat(outputview): outputview 클래스 추가
    - 실행 결과 헤더 출력 메서드 추가
    - 라운드별 자동차 상태를 '이름 : ----' 형식으로 출력
    - 최종 우승자를 쉼표로 합쳐 '최종 우승자 : name1, name2' 형식으로 출력

---
+++ src/main/java/racingcar/racing/ui/OutputView.java
@@ -0,0 +1,21 @@
+package racingcar.racing.ui;
+
+import java.util.List;
+import racingcar.racing.domain.Car;
+
+public class OutputView {
+    public void printExecutionHeader() {
+        System.out.println("실행 결과");
+    }
+
+    public void printRound(List<Car> cars) {
+        for (Car c : cars) {
+            System.out.println(c.render());
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}

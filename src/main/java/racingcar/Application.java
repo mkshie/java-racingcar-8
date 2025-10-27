ba665d1a3b21e06b974fb0e018a51ec447d961a7
junwoo <mkshie@naver.com>
2025-10-27 20:59:54 +0900

    feat(main): 프로그램 실행 로직 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.racing.domain.RacingGame;
+import racingcar.racing.ui.InputView;
+import racingcar.racing.ui.NameParser;
+import racingcar.racing.ui.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+
+        String rawNames = input.readNames();
+        int tryCount = input.readTryCount();
+
+        RacingGame game = new RacingGame(NameParser.parse(rawNames), tryCount);
+        output.printExecutionHeader();
+
+        game.playEachRound(output);
+        output.printWinners(game.findWinners());
+
     }
 }
